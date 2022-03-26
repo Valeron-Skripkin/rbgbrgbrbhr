72fb48dc8b7e460e3a04fb19250a2a60da0c5759
Valeron-Skripkin <89392872+Valeron-Skripkin@users.noreply.github.com>
2022-03-26 17:39:48 +0300

    Add files via upload

---
+++ homework js/script.js
@@ -0,0 +1,46 @@
+
+//Первое задание
+let country = 'Россия';
+    city = 'Новочеркасск';
+    population = 166.321;
+ console.log(country,city,population);   
+
+if(5 > 4) {
+    console.log ('Есть футбольный стадион');
+} 
+
+//Второе задание
+let width = 70;
+    height =40;
+    s = width * height;
+    console.log(s);
+
+//Третье задание
+let rezult = 418;
+ console.log(rezult);
+
+//Четвёртое задание
+const randomNumber = Math.floor(Math.random() * 100);
+if(randomNumber < 20) {
+    console.log("randomNumber меньше 20");
+} else if(randomNumber > 50) {
+    console.log("rundomNumber больше 50");
+} else {
+    console.log("randomNumber больше 20, и меньше 50");
+}
+
+//Пятое задание
+switch(randomNumber) {
+     case 'randomNumber < 20':
+         console.log("randomNumber меньше 20");
+        break;
+    case 'randomNumber > 50':
+        console.log("randomNumber больше 50"); 
+        break;
+    default:
+        console.log("randomNumber больше 20, и меньше 50");               
+ }
+    
+
+ 
+    
